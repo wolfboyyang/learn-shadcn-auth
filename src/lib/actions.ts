b5972429c6490f482a40fd9b3cd22dcd3634b37a
Wei Yang <wei.yang@rwth-aachen.de>
2025-04-08 08:57:33 +0200

    Grant permission for professor/admin team.

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,9 +2,11 @@
 
 import { stackServerApp } from "@/stack";
 
-export async function createUser(team_id: string, email: string) {
-  "use server";
+const ADMIN_TEAM_ID = process.env.ADMIN_TEAM_ID;
+const PROFESSOR_TEAM_ID = process.env.PROFESSOR_TEAM_ID;
+const STUDENT_TEAM_ID = process.env.STUDENT_TEAM_ID;
 
+export async function createUser(team_id: string, email: string) {
   const inviter = await stackServerApp.getUser({ or: "redirect" });
   const team = await stackServerApp.getTeam(team_id);
   if (!team) throw new Error("Team not found");
@@ -14,7 +16,7 @@ export async function createUser(team_id: string, email: string) {
   );
   if (!could_invite_member)
     throw new Error("You do not have permission to invite members");
-  await stackServerApp.createUser({
+  const user = await stackServerApp.createUser({
     primaryEmail: email,
     primaryEmailAuthEnabled: true,
     displayName: email,
@@ -24,5 +26,10 @@ export async function createUser(team_id: string, email: string) {
       inviter: inviter.id,
     },
   });
+
+  if (team.id === PROFESSOR_TEAM_ID || team.id === ADMIN_TEAM_ID) {
+    // Allow professor/admin invite professor/admin
+    user.grantPermission(team, "$invite_members");
+  }
   return true;
 }
